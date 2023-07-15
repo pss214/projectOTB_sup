8c4733856db98cc3ec8ff32bd3e604a002752eda
pss214 <bak10175@gmail.com>
2023-07-15 19:52:25 +0900

    spring 3.1.1 upgrade, login success

--- otbback/src/main/java/project/otb/service/UserService.java
+++ otbback/src/main/java/project/otb/service/UserService.java
@@ -1,29 +1,58 @@
 package project.otb.service;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import project.otb.DTO.LoginDto;
+import project.otb.DTO.UserDTO;
 import project.otb.entity.User;
 import project.otb.repositiry.UserRepository;
+
+import java.time.LocalDateTime;
 
 @Slf4j
 @Service
 public class UserService {
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public User create(final User entity){
-        if(entity == null || entity.getUsername() == null){
-            throw new RuntimeException("아이디를 사용할 수 있습니다.");
-        }
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User create(final UserDTO entity){
         final String username = entity.getUsername();
         if(userRepository.existsByUsername(username)){
             log.warn("username Already Exists!, {}", username);
-            throw new RuntimeException("아이디를 사용할 수 없습니다.");
+            throw new RuntimeException("아이디가 존재합니다!");
         }
-        return userRepository.save(entity);
+        User user= User.builder()
+                .password(passwordEncoder.encode(entity.getPassword()))
+                .username(entity.getUsername())
+                .email(entity.getEmail())
+                .nickname(entity.getNickname())
+                .Created_Date(LocalDateTime.now())
+                .build();
+
+        return userRepository.save(user);
     }
-    public User getByCredentials(final String username, final String password){
-        return userRepository.findByUsernameAndPassword(username,password);
+    public UserDTO getByCredentials(final LoginDto dto) {
+        User user = userRepository.findByUsername(dto.getUsername());
+        if (passwordEncoder.matches(dto.getPassword(), user.getPassword())) {
+//            final String token = tokenProvider.create(user);
+
+            final UserDTO response = UserDTO.builder()
+                    .username(user.getUsername())
+                    .email(user.getEmail())
+                    .nickname(user.getNickname())
+//                    .token(token)
+                    .build();
+//            log.info(tokenProvider.validateAndGetUserId(token));
+            return response;
+        }else {
+            return null;
+        }
+
     }
 }
--- project/src/main/java/otb/project/repositiry/BusRepository.java
+++
@@ -1,8 +0,0 @@
-package otb.project.repositiry;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import otb.project.entity.Bus;
-@Repository
-public interface BusRepository extends JpaRepository<Bus,String> {
-}
