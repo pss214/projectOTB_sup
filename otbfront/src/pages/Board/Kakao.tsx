95dffddf3e89b3237430ce4466481869b9d556d2
waz827 <waz827@naver.com>
2023-07-18 11:54:40 +0900

    modified:   otbfront/src/pages/Board/Kakao.tsx 	modified:   otbfront/src/routes/NoMatch.tsx
    Changes not staged for commit:
    	modified:   otbback/src/main/resources/application.properties
    	modified:   otbfront/src/contexts/AuthContext.tsx
    	modified:   otbfront/src/routes/Auth/Login.tsx
    	modified:   otbfront/src/routes/Auth/SignUp.tsx
    	modified:   otbfront/src/routes/LandingPage/Promotion.tsx
    	modified:   otbfront/src/routes/Layout/NavigationBar.tsx
    	modified:   otbfront/src/routes/RoutesSetup.tsx
    
    Untracked files:
    	otbfront/src/pages/Board/Traffik.tsx
    	otbfront/src/routes/Auth/SignUpDriver.tsx

--- otbfront/src/pages/Board/Kakao.tsx
+++ otbfront/src/pages/Board/Kakao.tsx
@@ -1,23 +1,25 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react'
 
-export {};
+export {}
 
 function Kakao(): JSX.Element {
   useEffect(() => {
-    const container = document.getElementById('map');
+    const container = document.getElementById('map')
     const options = {
       center: new (window as any).kakao.maps.LatLng(33.45701, 126.570667),
-      level: 3,
-    };
-    const map = new (window as any).kakao.maps.Map(container, options);
-  }, []);
+      level: 3
+    }
+    const map = new (window as any).kakao.maps.Map(container, options)
+  }, [])
 
   return (
-    <div id="map" style={{
-      width: '500px',
-      height: '500px'
-    }}></div>
-  );
+    <div
+      id="map"
+      style={{
+        width: '500px',
+        height: '500px'
+      }}></div>
+  )
 }
 
-export default Kakao;
+export default Kakao
