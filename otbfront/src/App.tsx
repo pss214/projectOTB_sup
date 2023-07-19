60a76c70fe50b95e5816868b779723231c62e9d8
DwKmm3 <128450146+DwKmm3@users.noreply.github.com>
2023-07-19 17:25:13 +0900

    Update App.tsx
    feat 사용자 위치 정보 이용 및 실패 시 문구

--- otbfront/src/App.tsx
+++ otbfront/src/App.tsx
@@ -1,22 +1,30 @@
-import {Provider as ReduxProvider} from 'react-redux'
-import {DndProvider} from 'react-dnd'
-import {HTML5Backend} from 'react-dnd-html5-backend'
-import {BrowserRouter} from 'react-router-dom'
-import {AuthProvider} from './contexts'
-import RoutesSetup from './routes/RoutesSetup'
-import {useStore} from './store'
+import { Provider as ReduxProvider } from 'react-redux';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './contexts';
+import RoutesSetup from './routes/RoutesSetup';
+import { useStore } from './store';
+import useGeoLocation from './useGeolocation';
 
 export default function App() {
-  const store = useStore()
+  const store = useStore();
+  const location = useGeoLocation();
+
   return (
     <ReduxProvider store={store}>
       <DndProvider backend={HTML5Backend}>
         <BrowserRouter>
           <AuthProvider>
+            <div className="App">
+              {location.loaded
+                ? JSON.stringify(location)
+                : "위치를 찾을 수 없습니다."}
+            </div>
             <RoutesSetup />
           </AuthProvider>
         </BrowserRouter>
       </DndProvider>
     </ReduxProvider>
-  )
+  );
 }
