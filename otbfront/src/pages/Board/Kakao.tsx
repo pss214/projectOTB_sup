d54ad9f786f283ec2d37dbd405073ad5bac95d1f
DwKmm3 <yywon24680@naver.com>
2023-07-19 16:31:42 +0900

    feat 사용자 위치 기반으로 지도 이동(버튼 추가하여 버튼으로 이용)

--- otbfront/src/pages/Board/Kakao.tsx
+++ otbfront/src/pages/Board/Kakao.tsx
@@ -1,22 +1,55 @@
-import React, { useEffect } from 'react';
-
-export {};
+import React, { useEffect, useState } from 'react';
 
 function Kakao(): JSX.Element {
+  const [map, setMap] = useState<any>(null);
+
   useEffect(() => {
     const container = document.getElementById('map');
     const options = {
-      center: new (window as any).kakao.maps.LatLng(33.45701, 126.570667),
+      center: new (window as any).kakao.maps.LatLng(37.5665, 126.9780), // 초기 위치 설정 (서울)
       level: 3,
     };
     const map = new (window as any).kakao.maps.Map(container, options);
+    setMap(map);
   }, []);
 
+  // 내 위치로 이동하는 함수
+  const moveToMyLocation = () => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const lat = position.coords.latitude;
+        const lng = position.coords.longitude;
+        const moveLatLng = new (window as any).kakao.maps.LatLng(lat, lng);
+        map.setCenter(moveLatLng); // 지도의 중심을 실시간 위치로 이동
+      },
+      (error) => {
+        console.error('Error getting geolocation:', error);
+      }
+    );
+  };
+
+
   return (
-    <div id="map" style={{
-      width: '500px',
-      height: '500px'
-    }}></div>
+    <div>
+    <div
+      id="map"
+      style={{ width: '500px', height: '500px' }}
+    ></div>
+    <button
+      style={{
+        backgroundColor: '#cddc39', // lime-500
+        color: '#fff',
+        borderRadius: '5px',
+        padding: '10px 20px',
+        fontSize: '16px',
+        border: 'none',
+        cursor: 'pointer',
+      }}
+      onClick={moveToMyLocation}
+    >
+      내 위치로 이동
+    </button>
+  </div>
   );
 }
 
