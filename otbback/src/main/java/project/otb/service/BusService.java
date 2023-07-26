c586e250eff38a043d77bdd4c95b8df4935f113d
pss214 <bak10175@gmail.com>
2023-07-26 13:43:29 +0900

    버스서비스 username 중복 여부 추가

--- otbback/src/main/java/project/otb/service/BusService.java
+++ otbback/src/main/java/project/otb/service/BusService.java
@@ -5,8 +5,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import project.otb.DTO.BusDTO;
 import project.otb.DTO.LoginDto;
-import project.otb.DTO.ResponseDTO;
 import project.otb.entity.Bus;
+import project.otb.repositiry.UserRepository;
 import project.otb.security.TokenProvider;
 import project.otb.repositiry.BusRepository;
 
@@ -18,26 +18,33 @@ public class BusService {
     private final BusRepository busRepository;
     private final PasswordEncoder passwordEncoder;
     private final TokenProvider tokenProvider;
+    private final UserRepository userRepository;
 
-    public BusService(BusRepository busRepository, PasswordEncoder passwordEncoder, TokenProvider tokenProvider) {
+    public BusService(BusRepository busRepository, PasswordEncoder passwordEncoder, TokenProvider tokenProvider, UserRepository userRepository) {
         this.busRepository = busRepository;
         this.passwordEncoder = passwordEncoder;
         this.tokenProvider = tokenProvider;
+        this.userRepository = userRepository;
     }
 
     public BusDTO create(final BusDTO entity) {
         if(busRepository.existsBybusNumberPlate(entity.getBusnumberplate())){
             throw new RuntimeException("아이디가 존재합니다!");
         }else {
-            Bus bus = Bus.builder()
-                    .BusNumber(entity.getBusnumber())
-                    .busNumberPlate(entity.getBusnumberplate())
-                    .Password(passwordEncoder.encode(entity.getPassword()))
-                    .Personnel(entity.getPersonnel())
-                    .Created_Date(LocalDateTime.now())
-                    .build();
-            busRepository.save(bus);
-            return null;
+            if(userRepository.existsByUsername(entity.getBusnumberplate())){
+                throw new RuntimeException("아이디가 존재합니다!");
+            }else{
+                Bus bus = Bus.builder()
+                        .BusNumber(entity.getBusnumber())
+                        .busNumberPlate(entity.getBusnumberplate())
+                        .Password(passwordEncoder.encode(entity.getPassword()))
+                        .Personnel(entity.getPersonnel())
+                        .Created_Date(LocalDateTime.now())
+                        .build();
+                busRepository.save(bus);
+                return null;
+            }
+
         }
     }
     public BusDTO getLogin(final LoginDto dto) {
