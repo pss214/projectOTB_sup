b0f39cce983c6d8c5aea63423150441c45397548
DwKmm3 <128450146+DwKmm3@users.noreply.github.com>
2023-08-03 15:04:39 +0900

    feat 수정하기 기능 추가

--- otbfront/src/pages/User/MyPage.tsx
+++ otbfront/src/pages/User/MyPage.tsx
@@ -1,52 +1,79 @@
-import React, {useState, useCallback} from 'react'
-import {SERVER_URL} from '../../server/getServer'
-import {Link} from '../../components'
-import * as U from '../../utils'
-import axios from 'axios'
-import type {ChangeEvent} from 'react'
+import React, { useState, useCallback } from 'react';
+import { SERVER_URL } from '../../server/getServer';
+import { Link } from '../../components';
+import * as U from '../../utils';
+import axios from 'axios';
+import type { ChangeEvent } from 'react';
 
 const MyPage: React.FC = () => {
-  const [user, setUser] = useState<any | null>(null)
-  const [jwt, setJwt] = useState<string>('')
-  const [loginData, setPassword] = useState({password: ''})
+  const [user, setUser] = useState<any | null>(null);
+  const [jwt, setJwt] = useState<string>('');
+  const [loginData, setPassword] = useState({ password: '' });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const {name, value} = e.target
-    setPassword(prevData => ({
+    const { name, value } = e.target;
+    setPassword((prevData) => ({
       ...prevData,
-      [name]: value
-    }))
-  }
-  type FormType = Record<'password', string>
-  const initialFormState = {password: ''}
+      [name]: value,
+    }));
+  };
+
+  type FormType = Record<'password', string>;
+  const initialFormState = { password: '' };
   const changed = useCallback(
     (key: string) => (e: ChangeEvent<HTMLInputElement>) => {
-      setForm(obj => ({...obj, [key]: e.target.value}))
+      setForm((obj) => ({ ...obj, [key]: e.target.value }));
     },
     []
-  )
-  const [{password}, setForm] = useState<FormType>(initialFormState)
+  );
+  const [{ password }, setForm] = useState<FormType>(initialFormState);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    U.readStringP('jwt').then(jwt => {
-      setJwt(jwt ?? '')
-    })
+    U.readStringP('jwt').then((jwt) => {
+      setJwt(jwt ?? '');
+    });
     axios(SERVER_URL + '/member', {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
-        Authorization: `otb ${jwt}`
-      }
+        Authorization: `otb ${jwt}`,
+      },
     })
-      .then(res => {
-        setUser(res.data.data[0])
-        console.log(res)
+      .then((res) => {
+        setUser(res.data.data[0]);
+        console.log(res);
       })
-      .catch(error => {
-        console.error('정보를 가져오는데 실패했습니다.', error)
-      })
-    e.preventDefault()
-  }
+      .catch((error) => {
+        console.error('정보를 가져오는데 실패했습니다.', error);
+      });
+    e.preventDefault();
+  };
+
+  const [showEditForm, setShowEditForm] = useState(false);
+
+  const toggleEditForm = () => {
+    setShowEditForm((prevState) => !prevState);
+  };
+
+  const [formData, setFormData] = useState({
+    newUsername: '',
+    newEmail: '',
+    // 다른 수정 정보들을 추가하세요
+  });
+
+  const handleFormChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleUpdate = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // 수정 정보를 서버로 전송하는 로직을 추가하세요
+    // axios 또는 fetch 등을 사용하여 서버로 수정 정보를 보낼 수 있습니다.
+  };
 
   return (
     <div>
@@ -65,12 +92,17 @@ const MyPage: React.FC = () => {
         <div className="flex flex-col items-center  flex-1 max-w-sm px-2 mx-auto">
           <div className="w-full px-6 py-8 text-black bg-white rounded shadow-md">
             <div>
-              <h1 className="mb-8 text-4xl text-center text-lime-500">마이 페이지</h1>
+              <h1 className="mb-8 text-4xl text-center text-lime-500">
+                마이 페이지
+              </h1>
               {user ? (
                 <div className="mb-8 text-2xl text-center text-black-500">
                   <p>Username: {user.username}</p>
                   <p>Email: {user.email}</p>
-                  <button className="flex-center ml-4 mr-4 btn btn-primary text-white  border-lime-600 bg-lime-600">
+                  <button
+                    className="flex-center ml-4 mr-4 btn btn-primary text-white  border-lime-600 bg-lime-600"
+                    onClick={toggleEditForm}
+                  >
                     수정하기
                   </button>
                   <button className="flex-center ml-4 mr-4 btn btn-primary text-white  border-lime-600 bg-lime-600">
@@ -88,10 +120,42 @@ const MyPage: React.FC = () => {
                   />
                   <button
                     className="btn btn-primary text-white bg-lime-500"
-                    type="submit">
+                    type="submit"
+                  >
                     로그인
                   </button>
                 </form>
+              )}
+              {showEditForm && (
+                <div>
+                  <h2 className="text-2xl text-center text-lime-500 mt-4">
+                    내 정보 수정하기
+                  </h2>
+                  <form className="mt-4" onSubmit={handleUpdate}>
+                    <input
+                      type="text"
+                      name="newUsername"
+                      placeholder="새로운 사용자 이름"
+                      value={formData.newUsername}
+                      onChange={handleFormChange}
+                      className="mt-2 p-2 border rounded"
+                    />
+                    <input
+                      type="email"
+                      name="newEmail"
+                      placeholder="새로운 이메일 주소"
+                      value={formData.newEmail}
+                      onChange={handleFormChange}
+                      className="mt-2 p-2 border rounded"
+                    />
+                    <button
+                      className="mt-4 btn btn-primary text-white bg-lime-500"
+                      type="submit"
+                    >
+                      저장하기
+                    </button>
+                  </form>
+                </div>
               )}
             </div>
             <Link to="/" className="btn btn-link text-lime-500">
@@ -101,7 +165,7 @@ const MyPage: React.FC = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default MyPage
+export default MyPage;
