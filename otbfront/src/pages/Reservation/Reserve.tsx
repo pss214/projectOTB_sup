4fe8e15a44e0abbc00ef11f73f2a557ffdf4f6a3
DwKmm3 <128450146+DwKmm3@users.noreply.github.com>
2023-08-09 21:29:01 +0900

    feat 버튼 추가 후 일정 갯수씩 잘라서 사용자가 보기 편하게 버스 목록 보여주기

--- otbfront/src/pages/Reservation/Reserve.tsx
+++ otbfront/src/pages/Reservation/Reserve.tsx
@@ -1,93 +1,111 @@
-import React, {useState, useEffect} from 'react'
-import {Link} from '../../components'
-import {useLocation} from 'react-router-dom'
-import * as U from '../../utils'
-import {SERVER_URL} from '../../server'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import { Link } from '../../components';
+import { useLocation } from 'react-router-dom';
+import * as U from '../../utils';
+import { SERVER_URL } from '../../server';
+import axios from 'axios';
 
 interface Bus {
-  rtNm: string
-  busRouteId:string
-  arrmsg1:string
-  arrmsg2:string
+  rtNm: string;
+  busRouteId: string;
+  arrmsg1: string;
+  arrmsg2: string;
 }
 
 interface BusStop {
-  id: number
-  place: string
-  lat: number
-  lng: number
+  id: number;
+  place: string;
+  lat: number;
+  lng: number;
 }
 
 interface ReservationFormProps {
-  buses: Bus[]
-  destinationStations: string[]
-  startingStation: string
-  onReservationSuccess: () => void
+  buses: Bus[];
+  destinationStations: string[];
+  startingStation: string;
+  onReservationSuccess: () => void;
 }
 
 const Reserve: React.FC<ReservationFormProps> = ({
   buses,
   destinationStations,
   startingStation,
-  onReservationSuccess
+  onReservationSuccess,
 }) => {
-  const [jwt, setJwt] = useState<string>('')
-  const [name, setName] = useState('')
-  const [seats, setSeats] = useState(1)
-  const [selectedBus, setSelectedBus] = useState<Bus | null>(null)
-  const [selectedDestination, setSelectedDestination] = useState('')
-  const [busArrivalInfo, setBusArrivalInfo] = useState([])
-  const location = useLocation()
-  const queryParams = new URLSearchParams(location.search)
-  const lat = queryParams.get('lat')
-  const lng = queryParams.get('lng')
-  const place = queryParams.get('place')
-  const id = queryParams.get('id')
+  const [jwt, setJwt] = useState<string>('');
+  const [name, setName] = useState('');
+  const [seats, setSeats] = useState(1);
+  const [selectedBus, setSelectedBus] = useState<Bus | null>(null);
+  const [selectedDestination, setSelectedDestination] = useState('');
+  const [busArrivalInfo, setBusArrivalInfo] = useState<Bus[]>([]); // 배열 타입으로 초기화
+  const [currentPage, setCurrentPage] = useState(1); // 페이지 관련 상태 추가
+  const itemsPerPage = 9;
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const lat = queryParams.get('lat');
+  const lng = queryParams.get('lng');
+  const place = queryParams.get('place');
+  const id = queryParams.get('id');
   const selectedMarker = location.state
-    ? (location.state as {selectedMarker: BusStop}).selectedMarker
-    : null
+    ? (location.state as { selectedMarker: BusStop }).selectedMarker
+    : null;
+
   useEffect(() => {
-    if(selectedMarker?.id){
+    if (selectedMarker?.id) {
       fetchData();
     }
-  }, [selectedMarker])
-  const fetchData =  () => {
+  }, [selectedMarker]);
+
+  const fetchData = () => {
     try {
-       fetch(SERVER_URL + '/bus/information', {
+      fetch(SERVER_URL + '/bus/information', {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
         },
-        body: JSON.stringify({id: selectedMarker?.id})
-      }).then((res)=>{
-        res.json().then(res=>{
-          console.log(res)
-            const data = res.data[0];
-            setBusArrivalInfo(data)
-        })
-      })
+        body: JSON.stringify({ id: selectedMarker?.id }),
+      }).then((res) => {
+        res.json().then((res) => {
+          console.log(res);
+          const data = res.data[0];
+          setBusArrivalInfo(data);
+        });
+      });
     } catch (error) {
-      console.error('API 요청 중 오류 발생:', error)
+      console.error('API 요청 중 오류 발생:', error);
     }
-  }
-  
+  };
+
+  const goToPreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const goToNextPage = () => {
+    if (indexOfLastItem < busArrivalInfo.length) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentBusArrivalInfo = busArrivalInfo.slice(indexOfFirstItem, indexOfLastItem);
 
   const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setName(e.target.value)
-  }
+    setName(e.target.value);
+  };
 
   const handleSeatsChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSeats(Number(e.target.value))
-  }
+    setSeats(Number(e.target.value));
+  };
 
   const handleDestinationChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedDestination(e.target.value)
-  }
-  
+    setSelectedDestination(e.target.value);
+  };
 
   const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault()
+    e.preventDefault();
 
     const reservationData = {
       startingStation,
@@ -95,38 +113,37 @@ const Reserve: React.FC<ReservationFormProps> = ({
       route: selectedBus?.busRouteId || '',
       destination: selectedDestination,
       name,
-      seats
-    }
+      seats,
+    };
 
-    const jwt = U.readStringP('jwt').then(jwt => {
-      setJwt(jwt ?? '')
-    })
+    const jwtPromise = U.readStringP('jwt').then((jwt) => {
+      setJwt(jwt ?? '');
+    });
 
-    // 서버로 예약 데이터 전송
     axios
       .post(SERVER_URL + '/reservation', reservationData, {
         headers: {
           'Content-Type': 'application/json',
-          Authorization: `otb ${jwt}`
-        }
+          Authorization: `otb ${jwt}`,
+        },
       })
-      .then(response => {
-        console.log('예약 성공:', response.data)
-        setName('')
-        setSeats(1)
-        setSelectedBus(null)
-        setSelectedDestination('')
-        onReservationSuccess() // 예약 성공 후 알림
+      .then((response) => {
+        console.log('예약 성공:', response.data);
+        setName('');
+        setSeats(1);
+        setSelectedBus(null);
+        setSelectedDestination('');
+        onReservationSuccess();
       })
-      .catch(error => {
-        console.error('예약 실패:', error)
-      })
-  }
+      .catch((error) => {
+        console.error('예약 실패:', error);
+      });
+  };
 
   return (
     <div>
       <div className="flex justify-between bg-lime-200">
-        <div className="flex p-2 ">
+        <div className="flex p-2">
           <Link to="/" className="ml-1">
             <img
               src="/img/otblogogogo.png"
@@ -153,15 +170,31 @@ const Reserve: React.FC<ReservationFormProps> = ({
             </div>
 
             <div>
-              <h2 className="mb-4 text-2xl text-lime-500">버스 도착 정보</h2>
-              {busArrivalInfo.length > 0 ? (
-                <ul>
-                  {busArrivalInfo.map((bus: any, index: number) => (
-                    <li key={index}>
-                     {bus.rtNm},{bus.arrmsg1}도착 예정
-                    </li>
-                  ))}
-                </ul>
+              {currentBusArrivalInfo.length > 0 ? (
+                <div>
+                  <h2 className="mb-4 text-2xl text-lime-500">버스 도착 정보</h2>
+                  <ul>
+                    {currentBusArrivalInfo.map((bus: any, index: number) => (
+                      <li key={index}>
+                        {bus.rtNm}, {bus.arrmsg1}, {bus.arrmsg2}
+                      </li>
+                    ))}
+                  </ul>
+                  <div className="flex justify-center mt-4">
+                  <button
+                    className="btn btn-orange mr-2" 
+                    onClick={goToPreviousPage}
+                    disabled={currentPage === 1}>
+                    이전
+                  </button>
+                  <button
+                    className="btn btn-orange" 
+                    onClick={goToNextPage}
+                    disabled={indexOfLastItem >= busArrivalInfo.length}>
+                    다음
+                  </button>
+                  </div>
+                </div>
               ) : (
                 <p>버스 도착 정보가 없습니다.</p>
               )}
