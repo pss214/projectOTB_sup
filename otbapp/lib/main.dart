c57cbe3453e26aee1036c19c3a32acc39c3d0432
DwKmm3 <128450146+DwKmm3@users.noreply.github.com>
2023-12-25 00:02:24 +0900

    feat: 메뉴 바, 슬라이딩 화면
    메뉴 버튼 클릭하면 좌측에서 메뉴 카테고리가 나오게 하였고, 뉴스 API 넣을 수 있도록 슬라이딩 화면 넣었습니다.
    
    메뉴 항목 클릭 시 해당 페이지로 이동 기능 추가 필요

--- otbapp/lib/main.dart
+++ otbapp/lib/main.dart
@@ -1,85 +1,158 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/cupertino.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-class Data{
-  List<Articles> articles;
+final dummyItems = [
+  'https://cdn.pixabay.com/photo/2018/11/12/18/44/thanksgiving-3811492_1280.jpg',
+  'https://cdn.pixabay.com/photo/2019/10/30/15/33/tajikistan-4589831_1280.jpg',
+  'https://cdn.pixabay.com/photo/2019/11/25/16/15/safari-4652364_1280.jpg',
+];
 
-  Data(this.articles);
-  factory Data.fromJson(dynamic json){
-    return Data(json['articles'] as List<Articles>);
-  }
+void main() => runApp(MyApp());
 
-  @override
-  String toString(){
-    return '{$articles}';
-  }
-
-}
-class Articles{
-  String title;
-  String description;
-
-  Articles(this.title,this.description);
-  factory Articles.fromJson(dynamic json){
-    return Articles(json['title'] as String, json['description'] as String);
-  }
-  @override
-  String toString(){
-    return '{$title,$description';
-  }
-}
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Menu Example2',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.lightGreen,
       ),
-      home: const MyHomePage(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('OTB'),
+          centerTitle: true,
+          elevation: 0.0,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.more_vert),
+              onPressed: () {},
+            )
+          ],
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              Container(
+                height: 120,
+                child: DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Colors.lightGreen,
+                  ),
+                  child: Text(
+                    '서비스 메뉴',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
+                  ),
+                ),
+              ),
+              ListTile(
+                title: Text('길찾기'),
+                onTap: () {},
+              ),
+              ListTile(
+                title: Text('버스 이용하기'),
+                onTap: () {},
+              ),
+              ExpansionTile(
+                title: Text('게시판'),
+                children: [
+                  ListTile(
+                    title: Text('공지사항'),
+                    onTap: () {},
+                  ),
+                  ListTile(
+                    title: Text('자유게시판'),
+                    onTap: () {},
+                  ),
+                ],
+              ),
+              ListTile(
+                title: Text('고객 센터'),
+                onTap: () {},
+              ),
+            ],
+          ),
+        ),
+        body: MyHomePage(),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-  void _callAPI() async {
-    var text = "Http Example";
-    List<Data> data = [];
-    var url = Uri.parse(
-      'https://back.otb.kr/news/&category=sports',
-    );
-    var response = await http.get(url);
-    text = utf8.decode(response.bodyBytes);
-    var dataObjsJson = jsonDecode(text)['data'] as List;
-    print('Response body: $dataObjsJson');
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
 
-    // url = Uri.parse('http://bak10172.asuscomm.com:10001');
-    // response = await http.post(url, body: {
-    //   'key': 'value',
-    // });
-    // print('Response status: ${response.statusCode}');
-    // print('Response body: ${response.body}');
-  }
+class _MyHomePageState extends State<MyHomePage> {
+  var _index = 0;
+  var _pages = [
+    Page1(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('http Example'),
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.add,
+              color: Colors.black,
+            ),
+            onPressed: () {},
+          ),
+        ],
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: _callAPI,
-          child: const Text('Call API'),
-        ),
+      body: _pages[_index],
+    );
+  }
+}
+
+class Page1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        _buildMiddle(),
+      ],
+    );
+  }
+
+  Widget _buildMiddle() {
+    return CarouselSlider(
+      options: CarouselOptions(
+        height: 150,
+        autoPlay: true,
       ),
+      items: dummyItems.map((url) {
+        return Builder(
+          builder: (BuildContext context) {
+            return Container(
+              width: MediaQuery.of(context).size.width,
+              margin: EdgeInsets.symmetric(horizontal: 5.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8.0),
+                child: Image.network(
+                  url,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            );
+          },
+        );
+      }).toList(),
     );
   }
 }
