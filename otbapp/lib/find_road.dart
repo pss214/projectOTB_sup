389db42bf843288a95f897bc3b40f67495bba727
DwKmm3 <128450146+DwKmm3@users.noreply.github.com>
2023-12-26 22:58:09 +0900

    feat: 메뉴 버튼 클릭 페이지 이동
    똑같은 동작 시키면 되는거라 일단 하나만 해두고 나머지는 로그인 연결 후 진행하겠습니다

---
+++ otbapp/lib/find_road.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NavigationPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('길찾기'),
+      ),
+      body: Center(
+        child: Text('길찾기 페이지입니다 API 가져와서 길찾기 추가해야합니다'),
+      ),
+    );
+  }
+}
