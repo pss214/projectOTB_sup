623854b9bda39953ae72dd946a93730917eb0d11
DwKmm3 <128450146+DwKmm3@users.noreply.github.com>
2023-12-27 16:58:53 +0900

    feat: 메뉴 카테고리 이동 페이지 추가

---
+++ otbapp/lib/free_board.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FreeBoardPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('자유게시판'),
+      ),
+      body: Center(
+        child: Text('자유게시판 입니다'),
+      ),
+    );
+  }
+}
