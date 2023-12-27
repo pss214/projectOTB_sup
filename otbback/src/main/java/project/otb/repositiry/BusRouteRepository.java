b223839fd937df17c507fdba4207054f0c550525
pss214 <bak10175@gmail.com>
2023-12-27 20:00:21 +0900

    종속성 설정

--- otbback/src/main/java/project/otb/repositiry/BusRouteRepository.java
+++ otbback/src/main/java/project/otb/repositiry/BusRouteRepository.java
@@ -1,8 +1,9 @@
 package project.otb.repositiry;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import project.otb.entity.BusRoute;
-
+@Repository
 public interface BusRouteRepository extends JpaRepository<BusRoute,Long> {
     BusRoute findByrouteidEquals(String Route_Id);
     BusRoute findByrouteEquals(String Route);
--- otbback/src/main/java/project/otb/repositiry/BusStationRepository.java
+++ otbback/src/main/java/project/otb/repositiry/BusStationRepository.java
@@ -1,8 +1,9 @@
 package project.otb.repositiry;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import project.otb.entity.BusStation;
-
+@Repository
 public interface BusStationRepository extends JpaRepository<BusStation, Long> {
     BusStation findBystationid(String stationid);
     BusStation findBystationuniid(String stationuniid);
