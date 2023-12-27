5f181e39e675301993d0683aebddd29ee8932c56
DwKmm3 <128450146+DwKmm3@users.noreply.github.com>
2023-12-27 16:58:57 +0900

    feat: 메뉴 카테고리 이동 페이지 추가

---
+++ otbapp/lib/reserveBus.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BusReservePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('버스 탑승하기'),
+      ),
+      body: Center(
+        child: Text('버스 페이지입니다'),
+      ),
+    );
+  }
+}
