05f2dd0a75a5b5c980f5d32254c39a4204dbc541
DwKmm3 <128450146+DwKmm3@users.noreply.github.com>
2024-01-07 00:18:57 +0900

    feat: 페이지 나누기 docs: 색상 변경

--- otbapp/lib/Bus_Select_Page.dart
+++ otbapp/lib/Bus_Select_Page.dart
@@ -4,13 +4,12 @@ import 'dart:convert';
 import 'Bus_Stop_Info.dart';
 import 'Bus_Total_Page.dart';
 
-
 class SelectPage extends StatefulWidget {
-  final String busNm; //routeName
-  final String busId; //busrouteid
-  final String busVehId;//vehId
-  final String arrivalMsg1; //arrmsg1
-  final String arrivalMsg2; //arrmsg2
+  final String busNm; // routeName
+  final String busId; // busrouteid
+  final String busVehId; // vehId
+  final String arrivalMsg1; // arrmsg1
+  final String arrivalMsg2; // arrmsg2
 
   SelectPage({
     required this.busNm,
@@ -21,33 +20,38 @@ class SelectPage extends StatefulWidget {
   });
 
   @override
-  _SelectPageState createState() => _SelectPageState(busNm:busNm, busId:busId, busVehId:busVehId);
+  _SelectPageState createState() => _SelectPageState(
+    busNm: busNm,
+    busId: busId,
+    busVehId: busVehId,
+  );
 }
 
-class _SelectPageState extends State<SelectPage>{
+class _SelectPageState extends State<SelectPage> {
   final String busId;
   final String busVehId;
   final String busNm;
-  _SelectPageState({required this.busNm ,required this.busId, required this.busVehId});
+  _SelectPageState(
+      {required this.busNm, required this.busId, required this.busVehId});
 
-  List<BusStop> datas=[];
+  List<BusStop> datas = [];
   String selectedStop = '';
 
-  dynamic fetchBus()async{
-    Map<String, String>headers = {
+  dynamic fetchBus() async {
+    Map<String, String> headers = {
       "Content-Type": "application/json",
       //"Authorization": "otb eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJiYWsxMDE3MjpST0xFX1VTRVIiLCJpc3MiOiJjb2xhYmVhcjc1NCIsImlhdCI6MTcwNDQyNDE3MSwiZXhwIjoxNzA0NDM0OTcxfQ.UuXXamzlmHLP07whzkpGra3du0tZh24uTmRwesMXQefimbiVasqohFJmzE_vDzMbazt5l_ce6dd6T_BeNqHt7g",
     };
-    var data={"busrouteid": busId,"stationid":"22014"}; //
-    var url=Uri.parse('http://bak10172.asuscomm.com:10001/bus/route-name');
+    var data = {"busrouteid": busId, "stationid": "22014"}; //
+    var url = Uri.parse('http://bak10172.asuscomm.com:10001/bus/route-name');
     var response = await http.post(
-        url,
-        body: json.encode(data),
-        headers: headers);
+        url, body: json.encode(data), headers: headers);
 
-    if(response.statusCode==200){
-      var dataObjsJson = jsonDecode(utf8.decode(response.bodyBytes))['data'][0] as List;
-      final List<BusStop> parsedResponse = dataObjsJson.map((a) => BusStop.fromJson(a)).toList();
+    if (response.statusCode == 200) {
+      var dataObjsJson =
+      jsonDecode(utf8.decode(response.bodyBytes))['data'][0] as List;
+      final List<BusStop> parsedResponse =
+      dataObjsJson.map((a) => BusStop.fromJson(a)).toList();
       setState(() {
         datas.clear();
         datas.addAll(parsedResponse);
@@ -66,59 +70,93 @@ class _SelectPageState extends State<SelectPage>{
     fetchBus();
   }
 
+  List<List<BusStop>> chunkPages(List<BusStop> list, int chunkSize) {
+    List<List<BusStop>> chunks = [];
+    for (var i = 0; i < list.length; i += 7) {
+      chunks.add(list.sublist(
+          i,
+          i + 7 > list.length
+              ? list.length
+              : i + 7));
+    }
+    return chunks;
+  }
+
   @override
   Widget build(BuildContext context) {
+    List<List<BusStop>> pages = chunkPages(datas, 7);
+
     return Scaffold(
-        appBar: AppBar(
-          title: Text('도착 정류장 목록'),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Padding(padding: EdgeInsets.only(top: 30)),
-              SizedBox(
-                height: 560,
-                child: ListView.builder(
-                  itemCount: datas.length,
-                  itemBuilder: (context,index){
-                    return Card(
-                      child: ListTile(
-                        onTap: (){
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context)=>TotalPage(
-                                    start: selectedStop,
-                                    arrive: datas[index].arsId,
-                                    vehId: busVehId,
-                                    busId: busNm,)
-                              )
-                          );
-                        },
-                        title: Text(datas[index].stationNm),
-                        subtitle: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text("정류장번호:${datas[index].arsId}"),
-                          ],
+      appBar: AppBar(
+        title: Text('도착 정류장 목록'),
+        backgroundColor: Colors.orangeAccent,
+      ),
+      body: PageView.builder(
+        itemCount: pages.length,
+        itemBuilder: (context, pageIndex) {
+          return SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(padding: EdgeInsets.only(top: 20)),
+                Text(
+                  "${pageIndex + 1}번째 페이지",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  "좌우로 넘겨 정류장을 선택하세요.",
+                  style: TextStyle(
+                    fontSize: 10,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                SizedBox(
+                  height: 1200,
+                  child: ListView.builder(
+                    itemCount: pages[pageIndex].length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        child: ListTile(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => TotalPage(
+                                      start: selectedStop,
+                                      arrive: pages[pageIndex][index].arsId,
+                                      vehId: busVehId,
+                                      busId: busNm,
+                                    )));
+                          },
+                          title: Text(pages[pageIndex][index].stationNm),
+                          subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("정류장번호:${pages[pageIndex][index].arsId}"),
+                            ],
+                          ),
                         ),
-                      ),
-                    );
-                  },
+                      );
+                    },
+                  ),
                 ),
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: TextButton(
-                  onPressed: (){
-                    fetchBus();
-                  },
-                  child: Text("통신상태확인용"),
-                ),
-              )
-            ],
-          ),
-        )
+                SizedBox(
+                  width: double.infinity,
+                  child: TextButton(
+                    onPressed: (){
+                      fetchBus();
+                    },
+                    child: Text("통신상태확인용"),
+                  ),
+                )
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
