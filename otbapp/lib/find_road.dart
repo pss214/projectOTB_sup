53d264fcf5fd3df60410276cd8d2c0867194f01c
waz827 <waz827@naver.com>
2024-01-06 20:03:44 +0900

    feat:내위치 기능 추가

--- otbapp/lib/find_road.dart
+++ otbapp/lib/find_road.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart' show rootBundle;
 
+class Bus_Stop{
+  final String stop_nm;
+  final String stop_no;
+  final double xcode;
+  final double ycode;
+
+  Bus_Stop({required this.stop_nm, required this.stop_no,
+            required this.xcode, required this.ycode});
+
+}
 class NavigationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -22,25 +35,81 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   late GoogleMapController mapController;
+  Location location = Location();
+  Set<Marker> markers ={};
 
-  final LatLng _center = const LatLng(37.4739388, 127.03374);//종열 어린이 공원
+  @override
+  void initState(){
+    super.initState();
+    _loadBusStops();
+  }
 
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
+  _onMapCreated(GoogleMapController controller) {
+    setState(() {
+      mapController = controller;
+      _getCurrentLocation();
+      _loadBusStops();
+    });
+  }
+
+  _getCurrentLocation() async {
+    try {
+      var currentLocation = await location.getLocation();
+      mapController.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(
+            target: LatLng(
+              currentLocation.latitude ?? 0.0,
+              currentLocation.longitude ?? 0.0,
+            ),
+            zoom: 11.0,
+          ),
+        ),
+      );
+    } catch (e) {
+      print("위치값을 가져오지 못했습니다.: $e");
+    }
+  }
+
+  _loadBusStops() async{
+    String jsonString = await rootBundle.loadString('assets/Bus_Stop_List.json');
+    List<dynamic>jsonList = json.decode(jsonString);
+
+    List<Bus_Stop>busStops=jsonList.map((json){
+      return Bus_Stop(
+          stop_nm: json['stop_nm'],
+          stop_no: json['stop_no'],
+          xcode: json['xcode'],
+          ycode: json['ycode']);
+    }).toList();
+
+    setState(() {
+      markers=busStops.map((busStops){
+        return Marker(
+          markerId: MarkerId(busStops.stop_no),
+          position: LatLng(busStops.xcode,busStops.ycode),
+          infoWindow: InfoWindow(title: busStops.stop_nm),
+        );
+      }).toSet();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
-          ),
+    return Scaffold(
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: LatLng(0.0, 0.0),
+          zoom: 11.0,
         ),
+        myLocationEnabled: true,
+        compassEnabled: true,
+        markers: markers,
       ),
     );
   }
+
+
+
 }
