59d57b7de0f7412cd20bc94784a4474a079c20da
DwKmm3 <128450146+DwKmm3@users.noreply.github.com>
2024-01-07 00:28:13 +0900

    refactor: 이전 코드 오류 다수 test: 길찾기 테스트 코드
    파일 위치도 잘못 올렸고 추가 설정해야 하는 부분들이 명시 되지 않아 해당 코드 커밋 되기 이전의 코드에서 작업한 테스트용 코드로 새로 커밋, 푸쉬합니다.

--- otbapp/lib/find_road.dart
+++ otbapp/lib/find_road.dart
@@ -1,19 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:location/location.dart';
+import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:flutter/services.dart' show rootBundle;
+import 'dart:async';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 
-class Bus_Stop{
-  final String stop_nm;
-  final String stop_no;
-  final double xcode;
-  final double ycode;
-
-  Bus_Stop({required this.stop_nm, required this.stop_no,
-            required this.xcode, required this.ycode});
-
-}
 class NavigationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -35,81 +26,202 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   late GoogleMapController mapController;
-  Location location = Location();
-  Set<Marker> markers ={};
+  final LatLng _center = const LatLng(37.4739388, 127.03374); // 종열 어린이 공원
 
-  @override
-  void initState(){
-    super.initState();
-    _loadBusStops();
+  final Set<Polyline> _polylines = {};
+  List<LatLng> polylineCoordinates = [];
+  late PolylinePoints polylinePoints;
+
+  TextEditingController _startLocationController = TextEditingController();
+  TextEditingController _destinationLocationController = TextEditingController();
+
+  String errorMessage = ''; //에러 메시지를 저장할 변수
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
   }
 
-  _onMapCreated(GoogleMapController controller) {
+  void showError(String message) {
     setState(() {
-      mapController = controller;
-      _getCurrentLocation();
-      _loadBusStops();
+      errorMessage = message;
+    });
+    //에러 메시지를 일정 시간 후에 지우기 위해 타이머 추가
+    Timer(Duration(seconds: 3), () {
+      setState(() {
+        errorMessage = '';
+      });
     });
   }
 
-  _getCurrentLocation() async {
+  Future<void> getPolylines() async {
     try {
-      var currentLocation = await location.getLocation();
-      mapController.animateCamera(
-        CameraUpdate.newCameraPosition(
-          CameraPosition(
-            target: LatLng(
-              currentLocation.latitude ?? 0.0,
-              currentLocation.longitude ?? 0.0,
-            ),
-            zoom: 11.0,
-          ),
-        ),
-      );
+      String apiKey = 'APIKEY';
+
+      LatLng sourceLocation = await getLocationCoordinates(_startLocationController.text);
+      LatLng destinationLocation = await getLocationCoordinates(_destinationLocationController.text);
+
+      String drivingApiUrl =
+          'https://maps.googleapis.com/maps/api/directions/json?origin=${sourceLocation.latitude},${sourceLocation.longitude}&destination=${destinationLocation.latitude},${destinationLocation.longitude}&mode=driving&key=$apiKey';
+
+      final drivingResponse = await http.get(Uri.parse(drivingApiUrl));
+      if (drivingResponse.statusCode == 200) {
+        Map<String, dynamic> drivingData = jsonDecode(drivingResponse.body);
+        List<dynamic> drivingRoutes = drivingData['routes'];
+        if (drivingRoutes.isNotEmpty) {
+          _polylines.clear();
+
+          Map<String, dynamic> drivingRoute = drivingRoutes[0];
+          Map<String, dynamic> drivingPolyline = drivingRoute['overview_polyline'];
+          String drivingPoints = drivingPolyline['points'];
+
+          polylineCoordinates = polylinePoints.decodePolyline(drivingPoints).cast<LatLng>();
+          List<LatLng> convertedDrivingCoordinates = [];
+
+          for (LatLng coordinate in polylineCoordinates) {
+            convertedDrivingCoordinates.add(LatLng(coordinate.latitude, coordinate.longitude));
+          }
+
+          setState(() {
+            Polyline drivingPolyline = Polyline(
+              polylineId: PolylineId('drivingPoly'),
+              color: Colors.blue,
+              points: convertedDrivingCoordinates,
+              width: 3,
+            );
+
+            _polylines.add(drivingPolyline);
+          });
+        }
+      }
+
+      String transitApiUrl =
+          'https://maps.googleapis.com/maps/api/directions/json?origin=${sourceLocation.latitude},${sourceLocation.longitude}&destination=${destinationLocation.latitude},${destinationLocation.longitude}&mode=transit&key=$apiKey';
+
+      final transitResponse = await http.get(Uri.parse(transitApiUrl));
+      if (transitResponse.statusCode == 200) {
+        Map<String, dynamic> transitData = jsonDecode(transitResponse.body);
+        List<dynamic> transitRoutes = transitData['routes'];
+        if (transitRoutes.isNotEmpty) {
+          Map<String, dynamic> transitRoute = transitRoutes[0];
+          Map<String, dynamic> transitPolyline = transitRoute['overview_polyline'];
+          String transitPoints = transitPolyline['points'];
+
+          polylineCoordinates = polylinePoints.decodePolyline(transitPoints).cast<LatLng>();
+          List<LatLng> convertedTransitCoordinates = [];
+
+          for (LatLng coordinate in polylineCoordinates) {
+            convertedTransitCoordinates.add(LatLng(coordinate.latitude, coordinate.longitude));
+          }
+
+          setState(() {
+            Polyline transitPolyline = Polyline(
+              polylineId: PolylineId('transitPoly'),
+              color: Colors.green,
+              points: convertedTransitCoordinates,
+              width: 3,
+            );
+
+            _polylines.add(transitPolyline);
+          });
+        }
+      }
     } catch (e) {
-      print("위치값을 가져오지 못했습니다.: $e");
+      // Handle errors
+      showError('정확한 장소를 입력해주세요.');
     }
   }
 
-  _loadBusStops() async{
-    String jsonString = await rootBundle.loadString('assets/Bus_Stop_List.json');
-    List<dynamic>jsonList = json.decode(jsonString);
 
-    List<Bus_Stop>busStops=jsonList.map((json){
-      return Bus_Stop(
-          stop_nm: json['stop_nm'],
-          stop_no: json['stop_no'],
-          xcode: json['xcode'],
-          ycode: json['ycode']);
-    }).toList();
+  Future<LatLng> getLocationCoordinates(String address) async {
+    try {
+      final apiKey = 'APIKEY';
+      final encodedAddress = Uri.encodeComponent(address);
 
-    setState(() {
-      markers=busStops.map((busStops){
-        return Marker(
-          markerId: MarkerId(busStops.stop_no),
-          position: LatLng(busStops.xcode,busStops.ycode),
-          infoWindow: InfoWindow(title: busStops.stop_nm),
-        );
-      }).toSet();
-    });
+      final apiUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address=$encodedAddress&key=$apiKey';
+      final response = await http.get(Uri.parse(apiUrl));
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        print('Geocoding response: $data'); //디버깅용 출력
+
+        final results = data['results'];
+
+        if (results.isNotEmpty) {
+          final location = results[0]['geometry']['location'];
+          final double latitude = location['lat'];
+          final double longitude = location['lng'];
+
+          return LatLng(latitude, longitude);
+        } else {
+          throw Exception('No results found for the provided address');
+        }
+      } else {
+        throw Exception('Failed to fetch location coordinates. Status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error in getLocationCoordinates: $e'); //디버깅용 출력
+      throw e; //에러를 상위로 전달
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    polylinePoints = PolylinePoints();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: GoogleMap(
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: CameraPosition(
-          target: LatLng(0.0, 0.0),
-          zoom: 11.0,
+    return MaterialApp(
+      home: Scaffold(
+        body: Stack(
+          children: [
+            GoogleMap(
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: CameraPosition(
+                target: _center,
+                zoom: 11.0,
+              ),
+              polylines: _polylines,
+            ),
+            //에러 메시지 표시
+            Positioned(
+              top: 16,
+              left: 16,
+              right: 16,
+              child: errorMessage.isNotEmpty
+                  ? Container(
+                padding: EdgeInsets.all(8),
+                color: Colors.red,
+                child: Text(
+                  errorMessage,
+                  style: TextStyle(color: Colors.white),
+                ),
+              )
+                  : Container(),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  TextField(
+                    controller: _startLocationController,
+                    decoration: InputDecoration(labelText: '출발지'),
+                  ),
+                  TextField(
+                    controller: _destinationLocationController,
+                    decoration: InputDecoration(labelText: '도착지'),
+                  ),
+                  ElevatedButton(
+                    onPressed: getPolylines,
+                    child: Text('길찾기'),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
-        myLocationEnabled: true,
-        compassEnabled: true,
-        markers: markers,
       ),
     );
   }
-
-
-
 }
