6bd38d50fad2e64da3b946c2ed27ffa05cf9b71d
DwKmm3 <128450146+DwKmm3@users.noreply.github.com>
2024-01-15 20:35:05 +0900

    feat: 사진 업로드 기능 추가

--- otbapp/lib/free_write.dart
+++ otbapp/lib/free_write.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
-import 'free_board.dart';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
 
-class FreeWritePage extends StatelessWidget {
+class FreeWritePage extends StatefulWidget {
   final Function(String) addFreeWrite;
 
   FreeWritePage(this.addFreeWrite);
 
+  @override
+  _FreeWritePageState createState() => _FreeWritePageState();
+}
+
+class _FreeWritePageState extends State<FreeWritePage> {
   final TextEditingController titleController = TextEditingController();
   final TextEditingController contentController = TextEditingController();
+  final picker = ImagePicker();
+  List<XFile?> images = [];
 
   @override
   Widget build(BuildContext context) {
@@ -36,13 +44,15 @@ class FreeWritePage extends StatelessWidget {
               ),
             ),
             SizedBox(height: 16.0),
+            ImageUpload(images, onImageDelete),
+            SizedBox(height: 16.0),
             ElevatedButton(
               onPressed: () {
                 String title = titleController.text;
                 String content = contentController.text;
 
-                //콜백 호출로 FreeBoardPage 목록에 제목 추가
-                addFreeWrite(title);
+                //콜백 호출로 제목 추가
+                widget.addFreeWrite(title);
 
                 Navigator.pop(context);
               },
@@ -53,4 +63,135 @@ class FreeWritePage extends StatelessWidget {
       ),
     );
   }
+
+  void onImageDelete(int index) {
+    setState(() {
+      images.removeAt(index);
+    });
+  }
 }
+
+class ImageUpload extends StatelessWidget {
+  final List<XFile?> images;
+  final Function(int) onDelete;
+
+  ImageUpload(this.images, this.onDelete);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 50,
+          ),
+          Row(
+            children: [
+              Container(
+                margin: EdgeInsets.all(10),
+                padding: EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                  color: Colors.lightBlueAccent,
+                  borderRadius: BorderRadius.circular(5),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 0.5,
+                      blurRadius: 5,
+                    ),
+                  ],
+                ),
+                child: IconButton(
+                  onPressed: () async {
+                    // Camera logic here
+                  },
+                  icon: Icon(
+                    Icons.add_a_photo,
+                    size: 30,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10),
+                padding: EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                  color: Colors.lightBlueAccent,
+                  borderRadius: BorderRadius.circular(5),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 0.5,
+                      blurRadius: 5,
+                    ),
+                  ],
+                ),
+                child: IconButton(
+                  onPressed: () async {
+                    // Gallery logic here
+                  },
+                  icon: Icon(
+                    Icons.add_photo_alternate_outlined,
+                    size: 30,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Container(
+            margin: EdgeInsets.all(10),
+            child: GridView.builder(
+              padding: EdgeInsets.all(0),
+              shrinkWrap: true,
+              itemCount: images.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                childAspectRatio: 1 / 1,
+                mainAxisSpacing: 10,
+                crossAxisSpacing: 10,
+              ),
+              itemBuilder: (BuildContext context, int index) {
+                return Stack(
+                  alignment: Alignment.topRight,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: FileImage(
+                            File(images[index]!.path),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: IconButton(
+                        padding: EdgeInsets.zero,
+                        constraints: BoxConstraints(),
+                        icon: Icon(
+                          Icons.close,
+                          color: Colors.white,
+                          size: 15,
+                        ),
+                        onPressed: () {
+                          onDelete(index);
+                        },
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}//https://ojlog.tistory.com/4 image_picker 참조
